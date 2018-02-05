7c1342a55190150ebef339a35a70de4f51a00623
Roberta Häggström <roberta.haggstrom@chasacademy.se>
2018-02-05 20:40:42 +0100

    ng generate component saved-recipes

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RecipeSearchComponent } from './recipe-search/recipe-search.component';
+import { SavedRecipesComponent } from './saved-recipes/saved-recipes.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeSearchComponent
+    RecipeSearchComponent,
+    SavedRecipesComponent
   ],
   imports: [
     BrowserModule
