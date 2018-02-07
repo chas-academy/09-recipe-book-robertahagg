7963028e097dc5ca5cb67ab5b8a730522acc77d3
Roberta Häggström <roberta.haggstrom@chasacademy.se>
2018-02-07 21:08:14 +0100

    First data binding succesfull test

--- recipe-book/src/app/recipe-search/recipe-search.component.ts
+++ recipe-book/src/app/recipe-search/recipe-search.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-search.component.css']
 })
 export class RecipeSearchComponent implements OnInit {
+  recipes: Recipe[];
 
   constructor() { }
 
   ngOnInit() {
+    this.recipes = [];
   }
 
+  search(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.recipes.push(new Recipe(name));
+  }
+}
+
+class Recipe {
+  id: number;
+  name: string;
+
+  constructor(public theName: string) {
+    this.name = theName;
+  }
 }
